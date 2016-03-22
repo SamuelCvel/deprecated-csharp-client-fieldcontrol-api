7335aa46faa16a93f509a203975c30b09103e9ea
Luiz Freneda <lfreneda@gmail.com>
2016-03-22 11:43:57 -0300

    Client unit tests check list

--- test/FieldControlApi.Tests/ClientTests.cs
+++ test/FieldControlApi.Tests/ClientTests.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 
 namespace FieldControlApi.Tests
 {
+    /*
+        [ ] Authenticate should set _authenticationToken when ok
+        [ ] Authenticate should throws AuthenticationException when fail
+
+        [ ] SetAuthenticationToken should set request.Token when _authenticationToken is not null
+        [ ] SetAuthenticationToken should throws ClientNotAuthenticatedException when _authenticationToken is null or empty
+
+        [ ] Send should call _httpRequester.ExecuteRequest
+    */
+
     [TestFixture]
     public class ClientTests
     {
