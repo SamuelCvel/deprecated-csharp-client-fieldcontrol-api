3d1cf6fb81a63519641b31e827ea25aeff457ad3
Luiz Freneda <lfreneda@gmail.com>
2016-03-22 16:38:30 -0300

    Route optimization request

---
+++ src/FieldControlApi/Requests/Activities/RouteOptimizationRequest.cs
@@ -0,0 +1,21 @@
+﻿using FieldControlApi.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldControlApi.Requests.Activities
+{
+    public class RouteOptimizationRequest : Request<RouteOptimizationResult>
+    {
+        public RouteOptimizationRequest(RouteOptimization routeOptimization) :
+            base(routeOptimization)
+        {
+
+        }
+
+        public override string ResourcePath { get { return "activities/actions/optimize-routes"; } }
+        public override string Method { get { return "POST"; } }
+    }
+}
---
+++ src/FieldControlApi/Resources/RouteOptimization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldControlApi.Resources
+{
+    public class RouteOptimizationResult : Resource
+    {
+        public bool Ok { get; set; }
+    }
+
+    public class RouteOptimization : Resource
+    {
+        public DateTime Date { get; set; }
+    }
+}
